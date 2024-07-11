4264d291e05aace131337f70d89fecc666738459
Nima Mahvi <147951809+Nima-Mahvi@users.noreply.github.com>
2024-07-11 19:23:59 +0330

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ class download:
             url = 'https://www.dropbox.com/scl/fi/4wxtzmgcm7yi66zge554u/complete_digits_data.csv?rlkey=jmje04sp07kyvc2h41t9bk1d5&st=j7i8id5r&dl=1'
             print("If this action is not done, you will be able to download it through the following link. Please put it next to the main program file after downloading: \n" , url)
             print("******************* \n It is a csv file about 207 MB. It contains 38206 rows and 2401 columns. Please check that the file has been downloaded correctly.")
-            print("******************* \n Downloading... \n Please waite")
+            print("******************* \n Downloading... \n Please wait")
             
             file_name = url.split('/')[-1].split('?')[0]
             root_path = os.path.dirname(os.path.abspath(__file__))
